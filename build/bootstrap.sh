947caf64aba714a1801586b76e2e326e3beb9a74
mishedone <michamilev@gmail.com>
2017-04-20 16:25:55 +0300

    Install MongoDB.

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -3,11 +3,16 @@
 # add repositories
 add-apt-repository ppa:ondrej/php
 curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | bash
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
+
+# setup mongo
+apt-get install -y mongodb-org
 
 # setup php
 apt-get install -y python-software-properties
-apt-get install -y php7.0 php7.0-fpm php7.0-cli php7.0-phalcon
+apt-get install -y php7.1 php7.1-fpm php7.1-cli php7.1-phalcon php7.1-mongo
 
 # setup composer
 apt-get install -y git unzip
