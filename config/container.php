dae3253ee08bf13aaa15c1b020a9dc787d2ef46c
mishedone <michamilev@gmail.com>
2017-05-21 12:44:10 +0300

    Create mongo configuration and service.

--- config/container.php
+++ config/container.php
@@ -7,6 +7,13 @@ $container = new Container();
 // global configuration
 $container->setShared('config', $config);
 
+// database connection
+$container->setShared('mongo', function () use ($config) {
+    $mongo = new MongoDB\Driver\Manager($config->mongo->uri);
+
+    return $mongo;
+});
+
 // resource context services
 $container->setShared('textToSpeechService', function () use ($config) {
     return new ResourceContext\Service\TextToSpeechService(
