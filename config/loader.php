d820f40141d0337a809ed5c18c86f4e57b964700
mishedone <michamilev@gmail.com>
2017-05-28 17:04:01 +0300

    Load incubator.

--- config/loader.php
+++ config/loader.php
@@ -5,6 +5,7 @@ use Phalcon\Loader;
 $loader = new Loader();
 
 $loader->registerNamespaces([
+    'Phalcon' => __DIR__ . '/../vendor/phalcon/incubator/Library/Phalcon/',
     'ResourceContext\Controller' => __DIR__ . '/../contexts/resource/controllers/',
     'ResourceContext\Service' => __DIR__ . '/../contexts/resource/services/'
 ])->register();
