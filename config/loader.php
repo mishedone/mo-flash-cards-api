ea02a6e97fe08eb9b6cc1a09374612a62ec9bb10
mishedone <michamilev@gmail.com>
2017-05-29 00:21:07 +0300

    Make contexts autoloadable.

--- config/loader.php
+++ config/loader.php
@@ -6,8 +6,6 @@ use Phalcon\Loader;
 $loader = new Loader();
 $loader->registerNamespaces([
     'Phalcon' => __DIR__ . '/../vendor/phalcon/incubator/Library/Phalcon/',
-    'ResourceContext\Controller' => __DIR__ . '/../contexts/resource/controllers/',
-    'ResourceContext\Model' => __DIR__ . '/../contexts/resource/models/',
-    'ResourceContext\Service' => __DIR__ . '/../contexts/resource/services/'
+    'ResourceContext' => __DIR__ . '/../contexts/ResourceContext/'
 ]);
 $loader->register();
