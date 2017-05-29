dbdbd0f533fcf4661a0fbd58c9b15adef5c2d512
mishedone <michamilev@gmail.com>
2017-05-29 20:39:49 +0300

    Load the deck context.

--- config/loader.php
+++ config/loader.php
@@ -6,6 +6,9 @@ use Phalcon\Loader;
 $loader = new Loader();
 $loader->registerNamespaces([
     'Phalcon' => __DIR__ . '/../vendor/phalcon/incubator/Library/Phalcon/',
+    
+    // contexts
+    'DeckContext' => __DIR__ . '/../contexts/DeckContext/',
     'ResourceContext' => __DIR__ . '/../contexts/ResourceContext/'
 ]);
 $loader->register();
